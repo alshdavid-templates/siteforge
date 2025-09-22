0fad13b413f3da12bc32a64622995fac41225d7e
David Alsh <alshdavid@gmail.com>
2025-09-22 11:48:57 +1000

    auth stuff

--- packages/client/src/gui/index.tsx
+++ packages/client/src/gui/index.tsx
@@ -1,35 +1,57 @@
 import "./styles.css";
 import { h, render } from "preact";
+import { useEffect, useState } from "preact/hooks";
+import { AuthClient } from "./auth.ts";
 
 new Worker(globalThis.importMap.resolve("worker")!, {
   type: "module",
 });
 
+const authClient = new AuthClient();
+
 function App() {
+  const [accountDetails, setAccountDetails] = useState(authClient.getDetails());
+
+  useEffect(() => {
+    authClient.onAuthAction(async (status, state) => {
+      console.warn(status, state);
+      setAccountDetails(authClient.getDetails());
+    });
+  }, [authClient]);
+
+  function login() {
+    authClient.navigateToLogin({ hello: "world" });
+  }
+
+  function logout() {
+    authClient.navigateToLogout();
+  }
+
+  async function refresh() {
+    await authClient.refreshAuth();
+  }
+
+  async function protectedApi() {
+    const resp = await authClient.validate();
+    console.log(resp);
+  }
+
   return (
     <div>
-      <h1>Website Template</h1>
-      <p>Basic starter template for bundled websites using Preact</p>
-      <p>
-        <b>It Features;</b>
+      <h1>Site Forge</h1>
+      <p id="status">
+        Status:{" "}
+        <span>
+          {accountDetails
+            ? `Logged in as ${accountDetails.email}`
+            : "Logged out"}{" "}
+        </span>
       </p>
-      <ul>
-        <li>Rspack bundler</li>
-        <li>TypeScript</li>
-        <li>Preact</li>
-        <li>CSS Modules/Nesting</li>
-        <li>Web Workers</li>
-        <li>Bundle Size Stats</li>
-        <li>Workflow to publish to GitHub Pages</li>
-        <li>
-          Template distributes 5kb of content (Preact, CSS, Worker + App code)
-        </li>
-      </ul>
-      <div>
-        <a href="https://github.com/alshdavid-templates/website-template">
-          GitHub Repo
-        </a>
-      </div>
+      <button onClick={login}>Login</button>
+      <button onClick={logout}>Logout</button>
+      <button onClick={refresh}>Refresh</button>
+      <button onClick={protectedApi}>Protected API</button>
+      <code></code>
     </div>
   );
 }
